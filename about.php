3ec18aacb5086751e29016500d03a2224b530f0f
AIC Digital <53875525+PWPankaj@users.noreply.github.com>
2023-04-15 12:52:14 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<h1>About US</h1>
+
+ 
+
+<?php
+
+ 
+
+echo "<h1>This App is created using Azure Web app service!!</h1>";
+
+ 
+
+?>
+
+  <a href="index.php">Home</a>
+
+</body>
+</html>
